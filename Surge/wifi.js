b7db1d28f97d9b66869200e1a3e8cec216307fd3
LisonChan <38126452+LisonChan@users.noreply.github.com>
2020-07-07 10:09:49 +0800

    Update wifi.js

--- Surge/wifi.js
+++ Surge/wifi.js
@@ -1,31 +1,22 @@
-/* wifi_proxy change
-[Script]
-SSID助手 = debug=1,script-path=https://raw.githubusercontent.com/rainman0925/GFW/master/Surge/Scripts/wifi_proxy.js,type=event,event-name=network-changed,control-api=true
+const SSID = $network.wifi.ssid;
+const proxywifi = !$persistentStore.read("lkWifiSsids")?["ssid填这里！！！","这是第二个ssid"]:JSON.parse($persistentStore.read("lkWifiSsids"));
+let res = proxywifi.some(val => val === SSID);
+let lkWifiLast = !$persistentStore.read("lkWifiLast")?"abcdefghijklmnopqrstuvwxyz":$persistentStore.read("lkWifiLast");
+if (lkWifiLast!=(!!SSID ? SSID : "cellular")){
+    !$persistentStore.write((!!SSID ? SSID : "cellular"), "lkWifiLast")
+    if (res) {
+        $surge.setOutboundMode("direct");
+        notify(1);
+    } else {
+        $surge.setOutboundMode("rule");
+        notify(0);
+    }
+}
 
-PS:记得自己修改WIFI名称
-主要功能:指定Wi-Fi(路由器翻)下,Surge使用直连模式,其他网络下Surge使用规则模式
-虽然设置SSID可以达到基本相同功能
-使用脚本,Surge不会被suspend
-Rewrite和Scripting依然有效
-*/
+function notify(mode) {
+    setTimeout(function () {
+        !!mode ? $notification.post("Wi-Fi assistant", "switch to direct mode", `your SSID is ${SSID}`) : $notification.post("Wi-Fi assistant", "switch to proxy mode", `${!!SSID ? "your SSID is " + SSID : "you are using cellular"}`)
+    })
+}
 
-var wifiname = $network.wifi.ssid;
-var proxywifi = ["pwhome"];
-for (var i = 0; i < proxywifi.length; i++) {
-	if (wifiname==proxywifi[i]){
-		$surge.setOutboundMode("direct");
-		
-		setTimeout(function(){$notification.post("Meeta_Remind","您目前处于WIFI-Proxy"+"SSID: "+wifiname,"Surge已自动变为直连模式");}, 3000);
-		break;
-		
-	};
-	if (i==proxywifi.length-1){
-		$surge.setOutboundMode("rule");
-		
-		setTimeout(function(){$notification.post("Meeta_Remind","Surge已自动变为规则模式","");}, 3000);
-	
-	}
-	
-	
-};
 $done();
